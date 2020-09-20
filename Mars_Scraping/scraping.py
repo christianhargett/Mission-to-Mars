6ec8bddf9ccc695353de5a5c0406544b5dda768e
Hargett <christian.hargett@accenturefederal.com>
2020-09-20 18:18:49 -0400

    Adding final code

--- Mars_Scraping/scraping.py
+++ Mars_Scraping/scraping.py
@@ -4,27 +4,26 @@ from bs4 import BeautifulSoup as soup
 import pandas as pd
 import datetime as dt
 
+
 def scrape_all():
     # Initiate headless driver for deployment
     browser = Browser("chrome", executable_path="chromedriver", headless=True)
 
     news_title, news_paragraph = mars_news(browser)
-
+    hemisphere_data = hemi_scrape(browser)
     # Run all scraping functions and store results in a dictionary
     data = {
         "news_title": news_title,
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemisphere_data
     }
 
     # Stop webdriver and return data
     browser.quit()
     return data
-
-
-
 
 
 def mars_news(browser):
@@ -86,7 +85,6 @@ def featured_image(browser):
 
     return img_url
 
-
 def mars_facts():
     # Add try/except for error handling
     try:
@@ -101,7 +99,27 @@ def mars_facts():
     df.set_index('Description', inplace=True)
 
     # Convert dataframe into HTML format, add bootstrap
-    return df.to_html()
+    return df.to_html(classes="table table-striped")
+
+def hemi_scrape(browser):
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+
+    hemisphere_image_urls = []
+
+    for i in range(4):
+        hemispheres = {}
+        browser.find_by_css('h3')[i].click()
+        sample_element = browser.find_link_by_text("Sample").first
+        hemispheres["img_url"] = sample_element["href"]
+        hemispheres["title"] = browser.find_by_css("h2.title").text
+
+        hemisphere_image_urls.append(hemispheres)
+    
+        browser.back()
+
+    return hemisphere_image_urls
+
 
 
 if __name__ == "__main__":
